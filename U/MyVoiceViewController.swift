786fa549bcefedfd84f1c7e4f77356080411f377
BrooksWon <jianyu996@163.com>
2016-05-05 14:38:00 +0800

    alert

--- U/MyVoiceViewController.swift
+++ U/MyVoiceViewController.swift
@@ -8,31 +8,127 @@
 
 import UIKit
 
+
+let kSuccessTitle = "U. 已收到您的呐喊"
+let kErrorTitle = "世界已崩溃！"
+let kNoticeTitle = "提示"
+let kWarningTitle = "警告"
+let kInfoTitle = "提示"
+let kSubtitle = "再呐喊一次 ？"
+
 class MyVoiceViewController: UIViewController {
 
     @IBAction func backBtnAction(sender: AnyObject) {
         self.navigationController?.popViewControllerAnimated(true)
     }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
 
+    @IBAction func commitBtnAction(sender: AnyObject) {
+//        showSuccess(sender)
+        showError(sender)
+    }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func showSuccess(sender: AnyObject) {
+        let alert = SCLAlertView()
+        alert.addButton("再来一次") {
+            print("Second button tapped")
+        }
+        alert.showSuccess(kSuccessTitle, subTitle: "")
     }
-    */
+    
+    @IBAction func showError(sender: AnyObject) {
+//        SCLAlertView().showError(kErrorTitle, subTitle:"You have not saved your Submission yet. Please save the Submission before accessing the Responses list. Blah de blah de blah, blah. Blah de blah de blah, blah.Blah de blah de blah, blah.Blah de blah de blah, blah.Blah de blah de blah, blah.Blah de blah de blah, blah.", closeButtonTitle:"OK")
+//                SCLAlertView().showError(self, title: kErrorTitle, subTitle: kSubtitle)
+        let alert = SCLAlertView()
+//        alert.addButton("再来一次") {
+//            print("Second button tapped")
+//        }
+        alert.showError(kErrorTitle, subTitle: "")
+    }
+    
+    @IBAction func showNotice(sender: AnyObject) {
+        SCLAlertView().showNotice(kNoticeTitle, subTitle: kSubtitle)
+    }
+    
+    @IBAction func showWarning(sender: AnyObject) {
+        SCLAlertView().showWarning(kWarningTitle, subTitle: kSubtitle)
+    }
+    
+    @IBAction func showInfo(sender: AnyObject) {
+        SCLAlertView().showInfo(kInfoTitle, subTitle: kSubtitle)
+    }
+    
+    @IBAction func showEdit(sender: AnyObject) {
+        let appearance = SCLAlertView.SCLAppearance(showCloseButton: true)
+        let alert = SCLAlertView(appearance: appearance)
+        let txt = alert.addTextField("Enter your name")
+        alert.addButton("Show Name") {
+            print("Text value: \(txt.text)")
+        }
+        alert.showEdit(kInfoTitle, subTitle:kSubtitle)
+    }
+    
+    
+    @IBAction func showCustomSubview(sender: AnyObject) {
+        // Create custom Appearance Configuration
+        let appearance = SCLAlertView.SCLAppearance(
+            kTitleFont: UIFont(name: "HelveticaNeue", size: 20)!,
+            kTextFont: UIFont(name: "HelveticaNeue", size: 14)!,
+            kButtonFont: UIFont(name: "HelveticaNeue-Bold", size: 14)!,
+            showCloseButton: false
+        )
+        
+        // Initialize SCLAlertView using custom Appearance
+        let alert = SCLAlertView(appearance: appearance)
+        
+        // Creat the subview
+        let subview = UIView(frame: CGRectMake(0,0,216,70))
+        let x = (subview.frame.width - 180) / 2
+        
+        // Add textfield 1
+        let textfield1 = UITextField(frame: CGRectMake(x,10,180,25))
+        textfield1.layer.borderColor = UIColor.greenColor().CGColor
+        textfield1.layer.borderWidth = 1.5
+        textfield1.layer.cornerRadius = 5
+        textfield1.placeholder = "Username"
+        textfield1.textAlignment = NSTextAlignment.Center
+        subview.addSubview(textfield1)
+        
+        // Add textfield 2
+        let textfield2 = UITextField(frame: CGRectMake(x,textfield1.frame.maxY + 10,180,25))
+        textfield2.secureTextEntry = true
+        textfield2.layer.borderColor = UIColor.blueColor().CGColor
+        textfield2.layer.borderWidth = 1.5
+        textfield2.layer.cornerRadius = 5
+        textfield1.layer.borderColor = UIColor.blueColor().CGColor
+        textfield2.placeholder = "Password"
+        textfield2.textAlignment = NSTextAlignment.Center
+        subview.addSubview(textfield2)
+        
+        // Add the subview to the alert's UI property
+        alert.customSubview = subview
+        alert.addButton("Login") {
+            print("Logged in")
+        }
+        
+        // Add Button with Duration Status and custom Colors
+        alert.addButton("Duration Button", backgroundColor: UIColor.brownColor(), textColor: UIColor.yellowColor(), showDurationStatus: true) {
+            print("Duration Button tapped")
+        }
+        
+        alert.showInfo("Login", subTitle: "", duration: 10)
+    }
 
 }
